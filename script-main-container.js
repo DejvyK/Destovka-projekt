61d7c8438bb5ca40cb9e06355ac2635838225480
Ladányi Kristóf <ladanyi.kristof@gmail.com>
2024-12-21 13:56:55 +0100

    Use calcualtor icon

--- script-main-container.js
+++ script-main-container.js
@@ -10,13 +10,18 @@ class DestovkaKonfigCalculator {
     initialize() {
         const volumeLabel = document.querySelector('.destovka-form-group:first-child .destovka-label');
         if (volumeLabel) {
-            const calcButton = document.createElement('button');
-            calcButton.className = 'destovka-konfig-calc-trigger';
-            calcButton.textContent = 'Nevím jak velkou nádrž potřebuji';
+            const calcButton = document.createElement('div');
+            calcButton.style.display = 'flex';
+            calcButton.style.cursor = 'pointer';
+            calcButton.innerHTML = `
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-calculator" viewBox="0 0 16 16">
+                <path d="M12 1a1 1 0 0 1 1 1v12a1 1 0 0 1-1 1H4a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1zM4 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h8a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2z"/>
+                <path d="M4 2.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-.5.5h-7a.5.5 0 0 1-.5-.5zm0 4a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3-6a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm3-6a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5zm0 3a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5z"/>
+            </svg>`;
             calcButton.onclick = () => this.showCalculator();
 
-            const inputRow = document.querySelector('.destovka-input-row');                      
-            inputRow.parentNode.insertBefore(calcButton, inputRow)
+            const labelWrapper = document.querySelector('.destovka-label-wrapper')
+            labelWrapper.appendChild(calcButton)
         }
 
         this.createCalculatorModal();
